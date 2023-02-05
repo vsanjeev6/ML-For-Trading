085b32167734265624ce7b86609a27adffd19e7a
Vaishnavi S <vaishnavisanjeev.22@gmail.com>
2023-02-05 22:07:10 +0100

    get_best_feature done

--- assess_learners/DTLearner.py
+++ assess_learners/DTLearner.py
@@ -1,76 +1,120 @@
 import numpy as np
-
 class DTLearner(object):
 
-    #Constructor
     def __init__(self, leaf_size=1, verbose=False):
         self.leaf_size = leaf_size
         self.verbose = verbose
-        #pass == return None
 
     def author(self):
-        return "vsanjeev6"
+        return 'vsanjeev6'
 
-    def add_evidence(self, data_x, data_y):
+    def addEvidence(self, dataX, dataY):
         """
-        Add training data to learner
+        @summary: Add training data to learner
+        @param dataX: X values of data to add
+        @param dataY: the Y training values
+        """
 
-        :param data_x: A set of feature values used to train the learner
-        :type data_x: numpy.ndarray
-        :param data_y: The value we are attempting to predict given the X data
-        :type data_y: numpy.ndarray
-        """
-        self.tree = self.build_tree(data_x,data_y)
+        self.tree = self.build_tree(dataX, dataY)
         if self.verbose:
             print("DTLearner")
-            print("Tree Shape" + str(self.tree.shape))
+            print("tree shape: " + str(self.tree.shape))
+            print("tree details below")
             print(self.tree)
-
 
     def query(self, points):
         """
-        Estimate a set of test points given the model we built.
-
-        :param points: A numpy array with each row corresponding to a specific query.
-        :type points: numpy.ndarray
-        :return: The predicted result of the input data according to the trained model
-        :rtype: numpy.ndarray
+        @summary: Estimate a set of test points given the model we built.
+        @param points: should be a numpy array with each row corresponding to a specific query.
+        @return: the estimated values according to the saved model.
         """
-        return (self.model_coefs[:-1] * points).sum(axis=1) + self.model_coefs[
-            -1
-        ]
+        out = []
+        for point in points:
+            out.append(self.get_prediction(point))
+        print("Predictions")
+        print(out)
+        return np.asarray(out)
 
-    def build_tree(self, data_x, data_y):
+    def get_prediction(self, point):
+        """
+        @summary: Predict one query using self.tree
+        @param point: numpy ndarray, one specific query.
+        @return: the prediction for the one query
+        """
 
+        node = 0
+        while ~np.isnan(self.tree[node][0]):
+            split_value = point[int(self.tree[node][0])]
+
+            # relative position so new_node = curr_node + offset
+            if split_value <= self.tree[node][1]:
+                node += int(self.tree[node][2])
+            else:
+                node += int(self.tree[node][3])
+            print("Query Tree")
+            print(self.tree[node][1])
+        return self.tree[node][1]
+
+    def get_best_feature(self, data_x, data_y):
         best_feature_index = 0
         best_corr_val = 0
 
-        #Stopping criteria
-        if data_x.shape[0] <= self.leaf_size:
-            return np.asarray([-1, np.mean(data_y), np.nan, np.nan])
-        if (all(y==data_y[0]) for y in data_y):
-            return np.asarray([-1, np.mean(data_y), np.nan, np.nan])
-
-        #Determine the best feature to split on based on correlation of Xi with Y
         for i in range(data_x.shape[1]):
-            correlation = np.corrcoef(data_x[:,i],data_y)
-            correlation = abs(correlation[0,1])
+            # Returns a 2x2 array where the diagonal elements are equal [[1,x],[x,1]]
+            correlation = abs(np.corrcoef(data_x[:, i], data_y)[0, 1])
+            # -1 and 1 are highly correlated, just in the opposite sense.
+            # Pick the first one that satifies this condition
             if correlation > best_corr_val:
                 best_corr_val = correlation
                 best_feature_index = i
+        return best_feature_index
 
-        #Computing the split value
-        split_val = np.median(data_x[:,best_feature_index])
+    def build_tree(self, dataX, dataY):
+        """
+        @summary: build the decision tree
+        @param dataX: numpy ndarray, features of trainning data
+        @param dataY: numpy ndarray, labels of tranning data
+        @return: numpy ndarray, decision tree in tabular format
+        """
 
-        #Build left and right trees
-        left_tree = self.build_tree(data_x[data_x[:,best_feature_index] <= split_val])
-        right_tree = self.build_tree(data_x[data_x[:,best_feature_index] > split_val])
+        # aggregated all the data left into a leaf if leaf_size or fewer entries left
+        if dataX.shape[0] <= self.leaf_size:
+            return np.asarray([np.nan, np.mean(dataY), np.nan, np.nan])
 
-        #Set root node (each sub-tree is a Decision tree itself)
-        root = np.asarray([best_feature_index, split_val, 1, left_tree.shape[0] + 1])
+        if np.all(np.isclose(dataY, dataY[0])):
+            return np.asarray([np.nan, dataY[0], np.nan, np.nan])
 
-        return np.append(root, left_tree, right_tree)
+        feature_index = self.get_best_feature(dataX, dataY)
+        split_val = np.median(dataX[:, feature_index])
+        print("Best Feature index", feature_index)
+        print("Split Val", split_val)
 
+        left_mask = dataX[:, feature_index] <= split_val
+        # make a leaf to prevent infinite recursion
+        if np.all(np.isclose(left_mask, left_mask[0])):
+            return np.asarray([np.nan, np.mean(dataY), np.nan, np.nan])
 
-if __name__ == "__main__":
-    print("the secret clue is 'zzyzx'")
+        right_mask = np.logical_not(left_mask)
+
+        """
+        # not correct! we should not delete a column becasue we can ask it again
+        dataX = np.delete(dataX, feature_index, 1)
+        """
+
+        left_tree = self.build_tree(dataX[left_mask], dataY[left_mask])
+
+        #print("++++++++")
+        #print(left_tree)
+        #print(left_tree.shape)
+
+        right_tree = self.build_tree(dataX[right_mask], dataY[right_mask])
+
+        if left_tree.ndim == 1:
+            root = np.asarray([feature_index, split_val, 1, 2])
+        else:
+            root = np.asarray([feature_index, split_val, 1, left_tree.shape[0] + 1])
+
+        return np.row_stack((root, left_tree, right_tree))
+
+    if __name__ == "__main__":
+        print('not implemented')
