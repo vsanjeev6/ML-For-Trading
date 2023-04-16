0cbd1a74d6f913a76bd516d4f37239c734529593
Vaishnavi S <vaishnavisanjeev.22@gmail.com>
2023-04-16 18:45:40 +0200

    Experiment 2 final

--- strategy_evaluation/experiment2.py
+++ strategy_evaluation/experiment2.py
@@ -10,7 +10,7 @@ import datetime as dt
 import pandas as pd
 import numpy as np
 from StrategyLearner import StrategyLearner
-import marketsimcode as ms
+import marketsimcode as mktsim
 import matplotlib.pyplot as plt
 
 """
@@ -26,31 +26,36 @@ def author():
     return 'vsanjeev6'
 
 def test_code():
-    sl = StrategyLearner(verbose=False, impact=0.0, commission=0.0)
-    sl.add_evidence(symbol='JPM', sd=dt.datetime(2008, 1, 1), ed=dt.datetime(2009, 12, 31), sv = 100000)
-    df_es1 = sl.testPolicy(symbol='JPM', sd=dt.datetime(2008, 1, 1), ed=dt.datetime(2009, 12, 31), sv=100000)
 
-    sl = StrategyLearner(verbose=False, impact=0.005, commission=0.0)
-    sl.add_evidence(symbol='JPM', sd=dt.datetime(2008, 1, 1), ed=dt.datetime(2009, 12, 31), sv = 100000)
-    df_es2 = sl.testPolicy(symbol='JPM', sd=dt.datetime(2008, 1, 1), ed=dt.datetime(2009, 12, 31), sv=100000)
+    learner1 = StrategyLearner(verbose=False, impact=0.0, commission=0.0)
+    learner1.add_evidence(symbol='JPM', sd=dt.datetime(2008, 1, 1), ed=dt.datetime(2009, 12, 31), sv = 100000)
+    df_trades1 = learner1.testPolicy(symbol='JPM', sd=dt.datetime(2008, 1, 1), ed=dt.datetime(2009, 12, 31), sv=100000)
+    portvals1 = mktsim.compute_portvals(df_trades1, start_val=100000, commission=0.0, impact=0.0)
+    portvals1 /= portvals1.values[0]
+    print_stats(portvals1)
 
-    sl = StrategyLearner(verbose=False, impact=0.1, commission=0.0)
-    sl.add_evidence(symbol='JPM', sd=dt.datetime(2008, 1, 1), ed=dt.datetime(2009, 12, 31), sv = 100000)
-    df_es3 = sl.testPolicy(symbol='JPM', sd=dt.datetime(2008, 1, 1), ed=dt.datetime(2009, 12, 31), sv=100000)
+    learner2 = StrategyLearner(verbose=False, impact=0.005, commission=0.0)
+    learner2.add_evidence(symbol='JPM', sd=dt.datetime(2008, 1, 1), ed=dt.datetime(2009, 12, 31), sv = 100000)
+    df_trades2 = learner2.testPolicy(symbol='JPM', sd=dt.datetime(2008, 1, 1), ed=dt.datetime(2009, 12, 31), sv=100000)
+    portvals2 = mktsim.compute_portvals(df_trades2, start_val=100000, commission=0.0, impact=0.005)
+    portvals2 /= portvals2.values[0]
+    print_stats(portvals2)
 
-    es1 = ms.compute_portvals(df_es1)
-    es2 = ms.compute_portvals(df_es2)
-    es3 = ms.compute_portvals(df_es3)
+    learner3 = StrategyLearner(verbose=False, impact=0.01, commission=0.0)
+    learner3.add_evidence(symbol='JPM', sd=dt.datetime(2008, 1, 1), ed=dt.datetime(2009, 12, 31), sv = 100000)
+    df_trades3 = learner3.testPolicy(symbol='JPM', sd=dt.datetime(2008, 1, 1), ed=dt.datetime(2009, 12, 31), sv=100000)
+    portvals3 = mktsim.compute_portvals(df_trades3, start_val=100000, commission=0.0, impact=0.01)
+    portvals3 /= portvals3.values[0]
+    print_stats(portvals3)
 
-    es1 /= es1.values[0]
-    es2 /= es2.values[0]
-    es3 /= es3.values[0]
-
+    """
+    Plot Graphs
+    """
     plt.title('Effect of Impact on In-Sample Trading Behaviour')
     plt.xticks(rotation=30)
-    plt.plot(es1, label="Impact = 0.0", color="red")
-    plt.plot(es2, label="Impact = 0.005", color="purple")
-    plt.plot(es3, label="Impact = 0.1", color="blue")
+    plt.plot(portvals1, label="Impact = 0.0", color="red")
+    plt.plot(portvals2, label="Impact = 0.005", color="purple")
+    plt.plot(portvals3, label="Impact = 0.01", color="blue")
     plt.xlabel("Date")
     plt.ylabel("Normalized Portfolio Value")
     plt.grid()
@@ -58,41 +63,28 @@ def test_code():
     plt.savefig("images/Exp2", dpi=300, bbox_inches='tight')
     plt.clf()
 
-def print_stats(portvals, benchmark):
-  benchmark, theoretical = benchmark['value'], portvals['value']
+def print_stats(portvals):
+  portfolio_value = portvals['value']
 
   # Cumulative Return
-  cr_ben = benchmark[-1] / benchmark[0] - 1
-  cr_the = theoretical[-1] / theoretical[0] - 1
+  cr = portfolio_value[-1] / portfolio_value[0] - 1
 
   # Daily Return
-  dr_ben = (benchmark / benchmark.shift(1) - 1).iloc[1:]
-  dr_the = (theoretical / theoretical.shift(1) - 1).iloc[1:]
+  dr = (portfolio_value / portfolio_value.shift(1) - 1).iloc[1:]
 
   # Stdev of daily returns
-  sddr_ben = dr_ben.std(ddof=1)
-  sddr_the = dr_the.std(ddof=1)
+  sddr = portfolio_value.std(ddof=1)
 
   # Mean of daily returns
-  adr_ben = dr_ben.mean()
-  adr_the = dr_the.mean()
+  adr = portfolio_value.mean()
 
   # Sharpe Ratio
-  sr_ben = np.sqrt(252) * (adr_ben / sddr_ben)
-  sr_the = np.sqrt(252) * (adr_the / sddr_the)
+  sr = np.sqrt(252) * (adr / sddr)
 
   print("~~~~~~~~~~Experiment 2~~~~~~~~~~")
-  print("======Manual Strategy======")
-  print("Cumulative Return: " + str(cr_the))
-  print("Stdev of daily returns: " + str(sddr_the))
-  print("Mean of daily returns: " + str(adr_the))
-  print("Sharpe Ratio:" + str(sr_the))
-  print("")
-  print("======Benchmark======")
-  print("Cumulative Return: " + str(cr_ben))
-  print("Stdev of daily returns: " + str(sddr_ben))
-  print("Mean of daily returns: " + str(adr_ben))
-  print("Sharpe Ratio:" + str(sr_ben))
+  print("Cumulative Return: " + str(cr))
+  print("Sharpe Ratio:" + str(sr))
 
 if __name__ == "__main__":
-    print("Experiment 2")
+    pass
+    #print("Experiment 2")
