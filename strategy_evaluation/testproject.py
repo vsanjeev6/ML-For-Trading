ba450874dc526c63d12a714e9fb4fcac847926cb
Vaishnavi S <vaishnavisanjeev.22@gmail.com>
2023-04-16 19:30:05 +0200

    testproject.py clean-up

--- strategy_evaluation/testproject.py
+++ strategy_evaluation/testproject.py
@@ -1,14 +1,25 @@
-import experiment1 as ex1
-import experiment2 as ex2
+"""
+Student Name: Vaishnavi Sanjeev
+GT User ID: vsanjeev6
+GT ID: 903797718
+
+Code implementing testproject.py
+"""
+
+import experiment1 as exp1
+import experiment2 as exp2
 import ManualStrategy as ms
 
 def author():
     return 'vsanjeev6'
 
 def run():
+    # Generates charts for Manual Strategy
     ms.test_code(symbol="JPM", sv=100000)
-    ex1.test_code()
-    ex2.test_code()
+    # Run Experiment 1
+    exp1.test_code()
+    # Run Experiment 2
+    exp2.test_code()
 
 if __name__ == "__main__":
     run()
